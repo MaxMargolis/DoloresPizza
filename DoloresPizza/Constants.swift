f2870a98815d0df85c9fe30bda2e1cf499aab79c
Max Margolis <maxbmargolis@gmail.com>
2020-07-06 11:33:32 -0700

    Constants

---
+++ DoloresPizza/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DoloresPizza
+//
+//  Created by Max Margolis on 7/6/20.
+//  Copyright © 2020 Max Margolis. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+	static let numberOfVenuesToRequest : Int = 10
+}
